a0f080d4c92e2ffe5f686741eab81d3f5de29549
Luísa Dornas <luisadvisentin@yahoo.com>
2024-10-01 20:29:58 -0300

    Create 1164.c

---
+++ 1164.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+   int n,x,i,j,sum;
+   sum=0;
+   
+   scanf("%d",&n);
+   for (i=1;i<=n;i++)
+   {
+       scanf("%d",&x);
+       for (j=1;j<x;j++)
+       {
+           if (x%j == 0)
+           {
+               sum+=j;
+           }
+       }
+       if (sum == x)
+       {
+           printf("%d eh perfeito\n",x);
+       }
+       else
+       {
+           printf("%d nao eh perfeito\n",x);
+       }
+       sum=0;
+   }
+    return 0;
+}
